dbb7f61c29137c3c1147adf234c8604f397c8db3
Alan Chelko <achelko@gmail.com>
2022-09-26 06:54:47 +0000

    Linked-list

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,33 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    // code
+  }
+
+  methodName() {
+    // code
+  }
 
 }
-
 module.exports = LinkedList;
+
+// Within your Linked List class, include a head property.
+// Upon instantiation, an empty Linked List should be created.
+
+// The class should contain the following methods
+
+// insert
+// Arguments: value
+// Returns: nothing
+// Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+// includes
+// Arguments: value
+// Returns: Boolean
+// Indicates whether that value exists as a Node’s value somewhere within the list.
+
+// to string
+// Arguments: none
+// Returns: a string representing all the values in the Linked List, formatted as:
+// "{ a } -> { b } -> { c } -> NULL"
