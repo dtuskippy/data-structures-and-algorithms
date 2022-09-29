5c235a6947530fd24becf7404e95bd4d15b98760
Alan Chelko <achelko@gmail.com>
2022-09-29 15:05:43 +0000

    Challenge 6 edits

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -50,6 +50,48 @@ class LinkedList {
     return string;
   }
 
+  append(value) {
+    let newNode = new Node(value);
+
+    if(!this.head){
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while(current.next){
+      current = current.next;
+    }
+    current.next = newNode;
+
+  }
+
+  insertBefore(value, newValue) {
+    if(!this.head){
+      throw new Error('Linked List is Empty');
+    }
+
+    if (this.head.value === value){
+      this.insert(newValue);
+      return;
+    }
+
+    let current = this.head;
+
+    while(current){
+      if(current.next && current.next.value === value) {
+        let newNode = new Node(newValue);
+        newNode.next = current.next;
+        current.next = newNode;
+        current = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+
+  }
+
 
 }
 
@@ -71,26 +113,21 @@ console.log('includes test', includes);
 
 console.log('To string', list.toString());
 
+list.append(16);
+console.log('Append', JSON.stringify(list));
+
+list.insertBefore(5, 17);
+console.log('insertBefore', JSON.stringify(list));
+
 
 
 module.exports = LinkedList;
 
-// Within your Linked List class, include a head property.
-// Upon instantiation, an empty Linked List should be created.
+// insert before
+// arguments: value, new value
+// adds a new node with the given new value immediately before the first node that has the value specified
 
-// The class should contain the following methods
 
-// insert
-// Arguments: value
-// Returns: nothing
-// Adds a new node with that value to the head of the list with an O(1) Time performance.
-
-// includes
-// Arguments: value
-// Returns: Boolean
-// Indicates whether that value exists as a Node’s value somewhere within the list.
-
-// to string
-// Arguments: none
-// Returns: a string representing all the values in the Linked List, formatted as:
-// "{ a } -> { b } -> { c } -> NULL"
+// insert after
+// arguments: value, new value
+// adds a new node with the given new value immediately after the first node that has the value specified
